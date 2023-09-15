7e7269f54ec5074728e46aa04f005d0074c7be2e
ValderiReis <139778381+ValderiReis@users.noreply.github.com>
2023-09-15 15:58:41 +0100

    Teoria e pratica aula 01

---
+++ Aula 01/hello-world.js
@@ -0,0 +1,37 @@
+// console.log("Ola Mundo")
+
+//let a = 5
+
+ a = "GRE"
+//console.log(a.length)
+
+// a = 10
+// console.log(a.length)
+
+
+//Let  a = "ISEL"
+//a = "ISEL"
+// let a = "ISEL";
+
+ // const a = "Valderi"
+ // a = "Valderi"
+
+
+
+var total = 0, count = 1;
+while (count <=10) {
+  total += count;
+  count += 1;
+}
+console.log(total);
+// → 55
+
+
+//let greeting = "say Hi";
+//let times = 4;
+
+//if (times > 3) {
+  //   let hello = "say Hello instead";
+    // console.log(hello);// dirá "say Hello instead"
+ //}
+//console.log(hello) // hello não está definido
