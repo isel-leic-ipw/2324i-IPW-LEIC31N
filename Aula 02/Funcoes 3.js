a89b50f361ed8f02caccffed787a5c10de74186c
ValderiReis <139778381+ValderiReis@users.noreply.github.com>
2023-09-25 16:43:12 +0100

    Class notes for 2 weeks weeks

--- Aula 02/Funcoes 3.js
+++ Aula 02/Funcoes 3.js
@@ -1,7 +1,43 @@
-function ola() {
-        console.log('Olá')
-        console.log('Turma')
-        console.log('IPW')
+  ///////////////////////////////////////////
+    
+//var arr = new Array(elemento0, elemento1, ..., elementoN);
+
+//var arr = Array(elemento0, elemento1, ..., elementoN);
+
+//var arr = [elemento0, elemento1, ..., elementoN];
+
+//const arr = [2,4,6,8];
+//const newArr = arr.map(num => num * 3);
+//console.log(newArr) 
+// saída: [4,8,12,16]
+
+
+//function ola() {
+  //      console.log('Olá')
+
+    //    console.log('Turma')
+
+    //    console.log('IPW')
     }
-    ola()
+    //ola()
+////////
+
+
+  function executaDe1a3(funcao) {
+    funcao(1);
+    funcao(2);
+    funcao(3);
+    
+  }
+  
+  executaDe1a3(function (x) { console.log('ISEL ' + x); });
+  
+    function Coelho(tipo) {
+        this.tipo = tipo;
+      }
+      
+      var coelhoAssassino = new Coelho("assassino");
+      var coelhoPreto = new Coelho("preto");
+      console.log(coelhoPreto.tipo);
+      // → preto
     
---
+++ Aula 02/function executa.js
@@ -0,0 +1,10 @@
+function executaDe1a3(funcao) {
+  funcao(1);
+  funcao(2);
+  funcao(3);
+}
+
+executaDe1a3(function (x) { console.log('Turma IPW' + x); });
+
+
+
