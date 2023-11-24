1ba14d7a8a357d51c866d680ce60fbd03f7900c0
ValderiReis <139778381+ValderiReis@users.noreply.github.com>
2023-11-24 19:27:43 +0000

    Class three notes

---
+++ Aula 03/Resoluções 1 Exercicio do Livro/3_triangle.js
@@ -0,0 +1,15 @@
+let character = ''
+const size = 9
+
+while (character.length < size) {
+    character += '*'
+    console.log(character)
+}
+//print a triangle
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
